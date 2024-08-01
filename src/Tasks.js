447521a56cb3e997177cc29205d305d1ce1df4db
sahil <sahil.saxena@student.manchester.ac.uk>
2024-08-01 19:51:00 +0100

    Implement dialog box

--- src/Tasks.js
+++ src/Tasks.js
@@ -28,56 +28,56 @@ export default function Task(
     completed,
   };
 }
-function Tasks() {
-  const saveProjects = () => {
-    localStorage.setItem("projects", JSON.stringify(Projects().projectList()));
-  };
+// function Tasks() {
+//   const saveProjects = () => {
+//     localStorage.setItem("projects", JSON.stringify(Projects().projectList()));
+//   };
 
-  const addTask = (title, description, date, priority, projectIndex) => {
-    const new_task = Task(title, description, date, priority, projectIndex);
-    const currentProject = Projects().projectList()[projectIndex];
+//   const addTask = (title, description, date, priority, projectIndex) => {
+//     const new_task = Task(title, description, date, priority, projectIndex);
+//     const currentProject = Projects().projectList()[projectIndex];
 
-    if (currentProject) {
-      Projects().projectList()[projectIndex].tasks.push(new_task);
-      saveProjects();
-      console.log(
-        "After adding task: ",
-        Projects().projectList()[projectIndex].tasks
-      );
-    } else {
-      console.error("Invalid project index");
-    }
-  };
+//     if (currentProject) {
+//       Projects().projectList()[projectIndex].tasks.push(new_task);
+//       saveProjects();
+//       console.log(
+//         "After adding task: ",
+//         Projects().projectList()[projectIndex].tasks
+//       );
+//     } else {
+//       console.error("Invalid project index");
+//     }
+//   };
 
-  const editTask = (
-    title,
-    description,
-    date,
-    priority,
-    projectIndex,
-    taskIndex
-  ) => {
-    Projects().projectList()[projectIndex].tasks[taskIndex].title = title;
-    Projects().projectList()[projectIndex].tasks[taskIndex].description =
-      description;
-    Projects().projectList()[projectIndex].tasks[taskIndex].date = date;
-    Projects().projectList()[projectIndex].tasks[taskIndex].priority = [
-      priority,
-    ];
-    saveProjects();
-  };
+//   const editTask = (
+//     title,
+//     description,
+//     date,
+//     priority,
+//     projectIndex,
+//     taskIndex
+//   ) => {
+//     Projects().projectList()[projectIndex].tasks[taskIndex].title = title;
+//     Projects().projectList()[projectIndex].tasks[taskIndex].description =
+//       description;
+//     Projects().projectList()[projectIndex].tasks[taskIndex].date = date;
+//     Projects().projectList()[projectIndex].tasks[taskIndex].priority = [
+//       priority,
+//     ];
+//     saveProjects();
+//   };
 
-  const deleteTask = (projectIndex, taskIndex) => {
-    if (projectIndex >= 0) {
-      Projects().projectList()[projectIndex].tasks.splice(taskIndex, 1);
-      saveProjects();
-    }
-  };
+//   const deleteTask = (projectIndex, taskIndex) => {
+//     if (projectIndex >= 0) {
+//       Projects().projectList()[projectIndex].tasks.splice(taskIndex, 1);
+//       saveProjects();
+//     }
+//   };
 
-  const changeTaskCompletion = (projectIndex, taskIndex) => {
-    const task = Projects().projectList()[projectIndex].tasks[taskIndex];
-    task.completed = !task.completed;
-    saveProjects();
-  };
-  return { addTask, editTask, deleteTask, changeTaskCompletion };
-}
+//   const changeTaskCompletion = (projectIndex, taskIndex) => {
+//     const task = Projects().projectList()[projectIndex].tasks[taskIndex];
+//     task.completed = !task.completed;
+//     saveProjects();
+//   };
+//   return { addTask, editTask, deleteTask, changeTaskCompletion };
+// }
